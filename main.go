510180929dfaf7fafb03514f06bfd1e11119e1c3
Brendan Ashton <brendan.ashton@getweave.com>
2024-10-17 14:08:07 -0600

    Brendan's suggestions added

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
-	"net/http"
-
 	"example.com/m/api"
+	"log"
+	"net/http"
 )
 
 func main() {
 	srv := api.NewServer()
-	http.ListenAndServe(":8080", srv)
+	err := http.ListenAndServe(":8080", srv)
+	if err != nil {
+		log.Fatalf("error starting server: %v", err)
+		// you should plan on always handling any errors in go unless you have a very good reason.
+		// If your app can't start serving the http server on a port, that is a critical startup failure and you should
+		// exit with a non-zero status code.
+		// Alternatively, you can use `panic(err)` to crash the program. Fine, but go panics are fairly rare and instill panic in me haha
+	}
 }
